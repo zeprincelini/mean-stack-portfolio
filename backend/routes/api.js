87625ae9ff48c85366eb09658704151322d084c5
Onwordi Prince <princelini4@gmail.com>
2020-09-14 01:58:41 +0100

    edited cloudinary-storage

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -58,8 +58,8 @@ cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET
-    });
-    const storage = cloudinaryStorage({
+ });
+    const storage = new cloudinaryStorage({
     cloudinary: cloudinary,
     folder: "portfolio-asset",
     allowedFormats: ["jpg", "png"],

1375c4625e40753cc7b06d6b88b8bd1853dad32f
Onwordi Prince <princelini4@gmail.com>
2020-09-17 00:58:54 +0100

    removed async

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -133,9 +133,9 @@ router.get("/dashboard", verifyToken, (req, res) => {
     })
 });
 
-router.post("/dashboard/add", async(req, res) => {
+router.post("/dashboard/add", (req, res) => {
         let imageData = req.body.img;
-        await cloudinary.uploader.upload(imageData,{folder: "portfolio-asset"}).then((result) => {
+        cloudinary.uploader.upload(imageData,{folder: "portfolio-asset"}).then((result) => {
         let mydate = new Date();
         let obj = new Post({
                 title: req.body.title,

797e088df6a1c13e8d409c0867ce1831c70b33f4
zeprincelini <princelini4@gmail.com>
2022-02-27 04:08:10 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component } from "@angular/core";
-import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
-import { filter, map, mergeMap } from "rxjs/operators";
+import { ThrowStmt } from "@angular/compiler";
+import { Component, Renderer2 } from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -10,19 +9,9 @@ import { filter, map, mergeMap } from "rxjs/operators";
 export class AppComponent {
   title = "myportfolio";
   root: Boolean;
-  public constructor(private route: ActivatedRoute, private router: Router) {}
-  ngOnInit() {
-    //   this.router.events.pipe(
-    //     filter(event => event instanceof NavigationEnd),
-    //     map(() => this.route),
-    //     map(route => {
-    //       while (route.firstChild) route = route.firstChild
-    //       return route
-    //     }),
-    //     filter(route => route.outlet === 'primary'),
-    //     mergeMap(route => route.data)
-    //   ).subscribe(data =>
-    //       this.root = data.showRootComponents
-    //     )
+  public constructor(private renderer: Renderer2) {}
+  ngAfterViewInit() {
+    let preLoader = this.renderer.selectRootElement(".pre-loader");
+    this.renderer.setStyle(preLoader, "display", "none");
   }
 }

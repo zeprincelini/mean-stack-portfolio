543cf5b287fcd42b4e3d5fab7f2d47cd64175bcf
zeprincelini <princelini4@gmail.com>
2022-02-27 04:41:03 +0100

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,55 +4,55 @@ import { NotFoundComponent } from "./components/not-found/not-found.component";
 import { MainLayoutComponent } from "./main-layout/main-layout.component";
 
 const routes: Routes = [
+  // {
+  //   path: "",
+  //   component: MainLayoutComponent,
+  //   children: [
+  //     {
+  //       path: "",
+  //       loadChildren: () =>
+  //         import("./modules/main/home-module/home.module").then(
+  //           (m) => m.HomeModule
+  //         ),
+  //     },
+  //     {
+  //       path: "skills",
+  //       loadChildren: () =>
+  //         import("./modules/main/skill-module/skill.module").then(
+  //           (m) => m.SkillModule
+  //         ),
+  //     },
+  //     {
+  //       path: "portfolio",
+  //       loadChildren: () =>
+  //         import("./modules/main/portfolio-module/portfolio.module").then(
+  //           (m) => m.PortfolioModule
+  //         ),
+  //     },
+  //     {
+  //       path: "about",
+  //       loadChildren: () =>
+  //         import("./modules/main/about-module/about.module").then(
+  //           (m) => m.AboutModule
+  //         ),
+  //     },
+  //     {
+  //       path: "contact",
+  //       loadChildren: () =>
+  //         import("./modules/main/contact-module/contact.module").then(
+  //           (m) => m.ContactModule
+  //         ),
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: "admin",
+  //   loadChildren: () =>
+  //     import("./modules/admin/admin.module").then((m) => m.AdminModule),
+  // },
   {
     path: "",
-    component: MainLayoutComponent,
-    children: [
-      {
-        path: "",
-        loadChildren: () =>
-          import("./modules/main/home-module/home.module").then(
-            (m) => m.HomeModule
-          ),
-      },
-      {
-        path: "skills",
-        loadChildren: () =>
-          import("./modules/main/skill-module/skill.module").then(
-            (m) => m.SkillModule
-          ),
-      },
-      {
-        path: "portfolio",
-        loadChildren: () =>
-          import("./modules/main/portfolio-module/portfolio.module").then(
-            (m) => m.PortfolioModule
-          ),
-      },
-      {
-        path: "about",
-        loadChildren: () =>
-          import("./modules/main/about-module/about.module").then(
-            (m) => m.AboutModule
-          ),
-      },
-      {
-        path: "contact",
-        loadChildren: () =>
-          import("./modules/main/contact-module/contact.module").then(
-            (m) => m.ContactModule
-          ),
-      },
-    ],
-  },
-  {
-    path: "admin",
-    loadChildren: () =>
-      import("./modules/admin/admin.module").then((m) => m.AdminModule),
-  },
-  {
-    path: "**",
-    pathMatch: "full",
+    // pathMatch: "full",
     component: NotFoundComponent,
   },
 ];
